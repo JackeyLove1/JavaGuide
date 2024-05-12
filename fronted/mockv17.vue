c634f0ae7027a0fa568dee05b901ce49eeb71f52
JackeyLove1 <1572727410@qq.com>
2024-05-12 20:03:58 +0800

    element-cascade

---
+++ fronted/mockv17.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="flex flex-col items-center">
+      <p>Child options expand when hovered</p>
+      <el-cascader
+          v-model="value"
+          :options="options"
+          :placeholder="placeHolder"
+          @change="handleChange"
+          clearable
+          class="mt-4"
+      />
+    </div>
+  
+  
+  </template>
+  
+  <script setup lang="ts">
+  import {ref} from "vue";
+  const value = ref([])
+  const placeHolder = ref("PlaceHolder")
+  const options = [
+    {
+      value: "value1",
+      label: "label1",
+      children: [
+        {
+          value: "value1-1",
+          label: "label1-1"
+        }
+      ]
+    }
+  ]
+  
+  function handleChange(value : any) : void {
+    console.log(value)
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ fronted/mockv18.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-col items-center justify-center gap-y-2">
+      <el-radio-group v-model="size" aria-label="size control">
+        <el-button value="large">large</el-button>
+        <el-button value="default">default</el-button>
+        <el-button value="small">small</el-button>
+      </el-radio-group>
+  
+      <span>Pick A Time</span>
+      <el-date-picker
+          v-model="value1"
+          type="date"
+          placeholder="Pick A Day"
+          :size="size"
+      />
+    </div>
+  
+  </template>
+  
+  <script setup lang="ts">
+  import {ref, watch} from "vue";
+  
+  const size = ref<'default' | 'large' | 'small'>('default')
+  const value1 = ref('')
+  watch(size, () => {
+    console.log(`size:${size}`)
+  })
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
