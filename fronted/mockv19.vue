1ed726563bf3bef933b29b7ea78532f56dfe7c68
JackeyLove1 <1572727410@qq.com>
2024-05-13 09:15:32 +0800

    el-switch

---
+++ fronted/mockv19.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex flex-col gap-y-2">
+      <div class="inline-flex items-center justify-center gap-x-2">
+        <span>username: </span>
+        <el-input
+            v-model="input"
+            placeholder="Input"
+            clearable
+            class="w-32"
+        />
+      </div>
+      <div class="inline-flex items-center justify-center gap-x-2">
+        <span>password: </span>
+        <el-input
+            v-model="pwd"
+            placeholder="password"
+            type="password"
+            clearable
+            class="w-32"
+            show-password
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import {ref, watch} from "vue";
+  
+  const input = ref('')
+  const pwd = ref('')
+  watch(input, () => {
+    console.log(`input: ${input.value}`)
+  })
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ fronted/mockv20.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <el-switch
+        v-model="value"
+        size="large"
+    >
+    </el-switch>
+  </template>
+  
+  <script setup lang="ts">
+  import {ref} from "vue";
+  
+  const value = ref(true)
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
