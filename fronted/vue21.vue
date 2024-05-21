de2b395661bd7911ab6a67b8fa851b3b84eb5a3e
JackeyLove1 <1572727410@qq.com>
2024-05-21 19:26:40 +0800

    chore: tailwind css practice

---
+++ fronted/vue21.vue
@@ -0,0 +1,83 @@
+<template>
+    <el-upload
+        v-model="fileList"
+        class="w-12 h-12 "
+    >
+    </el-upload>
+  
+    <el-badge :value="12" class="mt-4 ml-4">
+      <el-button> Commens</el-button>
+    </el-badge>
+  
+    <el-card class="w-48 text-center">
+      <template #header>
+        <div>Header</div>
+      </template>
+      <template #default>
+        <p
+            v-for="item in 5"
+            :key="item"
+            class="mb-2"
+        >
+          Item{{ item }}
+        </p>
+      </template>
+      <template #footer>
+        footer
+      </template>
+    </el-card>
+  
+    <el-pagination
+        :page-size="20"
+        :page-count="11"
+        layout="prev, pager, next"
+        :total="100"
+    >
+    </el-pagination>
+  
+    <el-progress
+        type="circle"
+        :percentage="0"
+    >
+    </el-progress>
+  
+    <el-result
+        icon="success"
+        title="Title"
+        sub-title="sub-title"
+    >
+      <template #extra>
+        <el-button type="primary">Back</el-button>
+      </template>
+    </el-result>
+  
+    <el-tag
+        type="primary"
+        closable
+    >
+      Tag1
+    </el-tag>
+  
+    <el-affix
+        :offset="120"
+    >
+      <el-button type="primary">Offset 120</el-button>
+    </el-affix>
+  
+    <el-breadcrumb :separator-icon="ArrowRight" class="mt-4">
+      <el-breadcrumb-item to="{path: '/'}">homepage</el-breadcrumb-item>
+      <el-breadcrumb-item >management</el-breadcrumb-item>
+    </el-breadcrumb>
+  </template>
+  
+  <script setup lang="ts">
+  import {ref} from 'vue'
+  import {ArrowRight} from "@element-plus/icons-vue";
+  
+  const fileList = ref([])
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ fronted/vue23.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-32">
+      <el-alert
+          :type="'success'"
+          :title="title"
+      >
+      </el-alert>
+    </div>
+  
+    <el-button
+        @click="handleOpen"
+    >
+      Show Dialog
+    </el-button>
+    <el-dialog
+        v-model="showDialog"
+        title="Warning"
+        class="w-[20rem]"
+    >
+      <span>FBI Warning</span>
+      <template #footer>
+        <div class="flex flex-row space-x-2">
+          <el-button @click="handleClose"> Cancel</el-button>
+          <el-button @click="handleClose"> Confirm</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  
+    <el-button @click="openElMessage"> Click Message</el-button>
+  
+    <el-button @click="openElNotification"> Click Notification</el-button>
+  </template>
+  
+  <script lang="ts" setup>
+  import {ref} from 'vue'
+  import {ElMessage, ElNotification} from "element-plus";
+  
+  const title = ref('title')
+  
+  const showDialog = ref(false)
+  
+  function handleClose() {
+    showDialog.value = false
+  }
+  
+  const handleOpen = () => showDialog.value = true
+  
+  const openElMessage = () => {
+    ElMessage("This is a message")
+  }
+  
+  const openElNotification = () => {
+    ElNotification({
+      type: "success",
+      message: "Notification",
+      title: "Success"
+    })
+  }
+  </script>
+  
+  <style>
+  </style>
+  
